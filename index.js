184ee483fbb990e3f5149b0ce28626b7c9b5a7d6
jaesea <oforkajajoseph@gmail.com>
2021-11-10 14:06:07 +0100

    updated index.js file

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const db = require('./Back-end/config/db');
 const userRouter = require('./Back-end/api/userRoutes');
 const adminRouter = require('./Back-end/api/adminRoutes');
 const cookieParser = require('cookie-parser');
-const path = require('path')
-
-//serving static files
-//app.use(express.static(__dirname + 'Front-end'));
-app.use(express.static(path.join(__dirname, 'Front-end')))
 
 //using Middlewear
 // var corsOptions = {
