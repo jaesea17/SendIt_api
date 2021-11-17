90626de3157e8eaee429b5ab11a42a4b5dac8dfd
jaesea <oforkajajoseph@gmail.com>
2021-11-17 16:19:24 +0100

    welcome page added

--- index.js
+++ index.js
@@ -20,5 +20,22 @@ app.use('/inventory', orderRouter);
 app.use('/user', userRouter);
 app.use('/admin',adminRouter);
 
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome to SendIT</h1>
+              <h3>SendIT is a courier service that helps users deliver parcels worldwide.
+              It provides courier quotes based on weight categories.</h3>
+              <p>For any more information please visit our
+              <a href='https://github.com/jaesea17/SendIt_api'>
+              Github repo!</a></p>
+              <h4>Thank you for visiting  &#x1F600;</h4>
+              `);
+  });
+
+app.get('*', (req, res) => httpResponse(res, {
+    statusCode: 400,
+    status: 'failure',
+    message: 'Oops! Not found',
+  }));
+
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`listening on port ${port}...`));
