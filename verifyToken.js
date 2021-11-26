21a1691a2ee98040ea3e57cf1f441f68468e1f17
jaesea <oforkajajoseph@gmail.com>
2021-11-26 14:00:24 +0100

    edited verify token file

--- verifyToken.js
+++ verifyToken.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next) =>{
-    //const token = req.header('auth_token');
-    const token = req.cookies.auth_token;
+    const token = req.header.auth_token;
     if(!token) return res.status(401).send("access denied");
     try{
         const verified = jwt.verify(token,process.env.TOKEN_SECRETE);
