cb245d0e0a28da1a4ea29fe4942ac54ea15be58e
jaesea <oforkajajoseph@gmail.com>
2021-11-26 02:08:49 +0100

    another commit

--- Back-end/config/db.js
+++ Back-end/config/db.js
@@ -5,7 +5,8 @@ let pool;
 if(process.env.NODE_ENV === "production"){
      pool = new Pool({
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: true,
+        rejectUnauthorized: false
     })
 }else{
     pool = new Pool({
