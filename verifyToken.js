5ad1532a00534aeb1fc901c1bf7db4e259ab3383
jaesea <oforkajajoseph@gmail.com>
2021-11-27 02:39:14 +0100

    another

--- verifyToken.js
+++ verifyToken.js
@@ -1,9 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next) =>{
-    const token = req.headers.Authorization; 
-    console.log("****req", req);
-    console.log("****req.header",req.headers);
+    const token = req.headers.authorization; 
+    console.log("****req.headers",req.headers);
     console.log("***",req.body);
     console.log("verifytoken:", token);
     if(!token) return res.status(401).send("access denied");
